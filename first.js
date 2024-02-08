692856076399bd26732fcfde5c30f8e97cf017e9
Hs chaudhary <hschaudhary06@gmail.com>
2024-02-08 12:55:46 +0530

    Day-1

---
+++ first.js
@@ -0,0 +1,9 @@
+var calulator = {
+    add : function(a,b){
+        return `${a} + ${b} = ${a + b}`;
+    },
+    sub : function(a,b){
+        return `${a} - ${b} = ${a - b}`;
+    },
+    multi : "it's not supported"
+}
