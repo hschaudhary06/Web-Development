5928e59a67b3f6abc2c4b00e7c92419cdbc05e8f
Hs chaudhary <hschaudhary06@gmail.com>
2024-02-09 18:25:05 +0530

    Day-2 final

--- first.js
+++ first.js
@@ -1,9 +1,34 @@
-var calulator = {
-    add : function(a,b){
-        return `${a} + ${b} = ${a + b}`;
-    },
-    sub : function(a,b){
-        return `${a} - ${b} = ${a - b}`;
-    },
-    multi : "it's not supported"
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+// circle mouse follower
+function circleMouseFollower(){
+    window.addEventListener("mousemove",function (dets) {
+        document.querySelector('#minicircle').style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    });
 }
+circleMouseFollower()
+
+//animation text
+
+function firstPageTextAnimation(){
+    var tl = gsap.timeline();
+
+    tl.from("#nav",{
+        y: '-10px',
+        opacity: 0,
+        duration: 1,
+        ease: Expo.easeInOut
+    })
+    .to(".boundingelem",{
+        y: 0,
+        duration: 1,
+        ease: Expo.easeInOut,
+        stagger: .2
+    })
+}
+
+firstPageTextAnimation()
